69de0bedc1cf194fb90448364311ffb36315e02d
SGANigz <frans_vanthek@live.nl>
2018-02-24 01:40:20 +0100

    changed NAME in Reference.java to WorstModEver

--- src/main/java/sirglitchalot/worstmodever/Reference.java
+++ src/main/java/sirglitchalot/worstmodever/Reference.java
@@ -3,7 +3,7 @@ package sirglitchalot.worstmodever;
 public class Reference {
 
 	public static final String MOD_ID = "wme";
-	public static final String NAME = "NewMod";
+	public static final String NAME = "WorstModEver";
 	public static final String VERSION = "1.0";
 	
 	public static final String SERVER_PROXY_CLASS = "sirglitchalot.worstmodever.proxy.CommonProxy";
