4efac6e40e0ee03655fb2256af2534fdb468a14d
F.S.S. van 't Hek <frans.van.t.hek@hva.nl>
2018-03-12 13:03:15 +0100

    made some small changes, shit dtill doesnt work.

--- src/main/java/com/sirglitchalot/wme/proxy/ClientProxy.java
+++ src/main/java/com/sirglitchalot/wme/proxy/ClientProxy.java
@@ -17,11 +17,15 @@ public class ClientProxy extends CommonProxy{
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		OBJLoader.INSTANCE.addDomain(Reference.MODID);
-		registerModel(ModItems.bagel);
+		registerModels();
 	}
 	
 	public void registerModel(Item item) {
-		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(Reference.MODID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(Reference.MODID.toLowerCase() + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
+	
+	public void registerModels() {
+		registerModel(ModItems.bagel);
 	}
 	
 	@Override
